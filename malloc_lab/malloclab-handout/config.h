31e523adfdbe883e204c7439b2373b78c812c897
ZHJD <2262040671@qq.com>
2020-03-29 15:52:47 +0800

    更改trace文件路径，mdriver没有读取在线文件

--- malloc_lab/malloclab-handout/config.h
+++ malloc_lab/malloclab-handout/config.h
@@ -12,7 +12,7 @@
  * This is the default path where the driver will look for the
  * default tracefiles. You can override it at runtime with the -t flag.
  */
-#define TRACEDIR "/afs/cs/academic/class/15213-s03/labs-save/L6/traces"
+#define TRACEDIR "./traces/"
 
 /*
  * This is the list of default tracefiles in TRACEDIR that the driver
